5f2c29cff9d2208d9d78e9b0417447cd5481334c
Naum <naum.khenkin@bk.ru>
2024-12-19 20:57:41 +0200

    фыв

--- calc.go
+++ calc.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"net/http"
 	"strconv"
 )
+
+type Request struct {
+	Expression string `json:"expression"`
+}
+
+type Response struct {
+	Result string `json:"result,omitempty"`
+	Error  string `json:"error,omitempty"`
+}
 
 func isOperator(c byte) bool {
 	return c == '+' || c == '-' || c == '*' || c == '/'
@@ -44,28 +56,6 @@ func Calc(expression string) (float64, error) {
 		if expression[i] == ' ' {
 			i++
 			continue
-		} else if expression[i] == '(' {
-			opStack = append(opStack, expression[i])
-		} else if expression[i] == ')' {
-			for len(opStack) > 0 && opStack[len(opStack)-1] != '(' {
-				op := opStack[len(opStack)-1]
-				opStack = opStack[:len(opStack)-1]
-
-				if len(numStack) < 2 {
-					return 0, errors.New("invalid expression")
-				}
-				b := numStack[len(numStack)-1]
-				numStack = numStack[:len(numStack)-1]
-				a := numStack[len(numStack)-1]
-				numStack = numStack[:len(numStack)-1]
-
-				result, err := applyOp(a, b, op)
-				if err != nil {
-					return 0, err
-				}
-				numStack = append(numStack, result)
-			}
-			opStack = opStack[:len(opStack)-1]
 		} else if isOperator(expression[i]) {
 			for len(opStack) > 0 && priority(opStack[len(opStack)-1]) >= priority(expression[i]) {
 				op := opStack[len(opStack)-1]
@@ -125,4 +115,39 @@ func Calc(expression string) (float64, error) {
 	}
 
 	return numStack[0], nil
+}
+
+func calculateHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Unsupported method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req Request
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil || req.Expression == "" {
+		http.Error(w, "Expression is not valid", http.StatusUnprocessableEntity)
+		return
+	}
+
+	result, err := Calc(req.Expression)
+	if err != nil {
+		if err.Error() == "division by zero" {
+			http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+			return
+		}
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	resp := Response{Result: strconv.FormatFloat(result, 'f', -1, 64)}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(resp)
+}
+
+func main() {
+	http.HandleFunc("/api/v1/calculate", calculateHandler)
+	fmt.Println("Сервер запущен на порту 8080")
+	http.ListenAndServe(":8080", nil)
 }
